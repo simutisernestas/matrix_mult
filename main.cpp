f0f75c2a7d59d06fcb46b420470ce15ba8342ca6
Ernestas <ernestas@neurotechnology.com>
2020-05-25 16:32:54 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ typedef struct Matrix_d
 {
     size_t R, C;
     // Indexing - arr[i*sizeY+j] == arr[i][j]
-    int* index;
+    int *index;
 
     ~Matrix_d()
     {
@@ -67,7 +67,8 @@ void staticProduct()
     for (size_t i = 0; i < m2.R * m2.C; i++)
         input >> m2.index[i / m2.C][i % m2.C];
 
-    if (m1.C != m2.R) {
+    if (m1.C != m2.R)
+    {
         cout << "Error: matrices cannot be multiplied!" << endl;
         return;
     }
@@ -77,16 +78,19 @@ void staticProduct()
     m3.R = m1.R;
     m3.C = m2.C;
     int sum{};
-    for (size_t i = 0; i < m3.R * m3.C; i++) {
+    for (size_t i = 0; i < m3.R * m3.C; i++)
+    {
         sum = 0;
-        for (size_t j = 0; j < m1.C; j++) {
+        for (size_t j = 0; j < m1.C; j++)
+        {
             sum += m1.index[i / m3.C][j % m1.C] * m2.index[j][i % m3.C];
         }
         m3.index[i / m3.C][i % m3.C] = sum;
     }
 
     // Print matrix
-    for (size_t i = 1; i <= m3.R * m3.C; i++) {
+    for (size_t i = 1; i <= m3.R * m3.C; i++)
+    {
         cout << m3.index[(i - 1) / m3.C][(i - 1) % m3.C] << '\t';
         if (i % 5 == 0)
             cout << endl;
@@ -97,7 +101,7 @@ void staticProduct()
 
 void dynamicProduct()
 {
-    fstream input("../matrix.2.in", ios::in);
+    fstream input("../matrix2.11.in", ios::in);
 
     // Read matrix
     MatrixD m1;
@@ -115,9 +119,26 @@ void dynamicProduct()
     for (size_t i = 0; i < m2.R * m2.C; i++)
         input >> m2.index[i];
 
+    MatrixD m2T;
+    m2T.R = m2.R;
+    m2T.C = m2.C;
+    m2T.index = new int[m2.C * m2.R];
+
+    // Transpose m2
+    int i{0};
+    for (size_t r = 0; r < m2.C; ++r)
+    {
+        for (size_t c = 0; c < m2.R; ++c)
+        {
+            m2T.index[i] = m2.index[r + c * m2.C];
+            ++i;
+        }
+    }
+
     input.close();
 
-    if (m1.C != m2.R) {
+    if (m1.C != m2.R)
+    {
         cout << "Error: matrices cannot be multiplied!" << endl;
         return;
     }
@@ -127,24 +148,28 @@ void dynamicProduct()
     m3.R = m1.R;
     m3.C = m2.C;
     m3.index = new int[m3.C * m3.R];
-    int sum{};
-    for (size_t r = 0; r < m3.R; ++r) {
-        int ri2 = m2.C * r;
-        int ri1 = m1.C * r;
-        for (size_t c = 0; c < m3.C; ++c) {
+    size_t nRows = m3.R, nCols = m3.C, nInner = m1.C;
+    int sum{0};
+    for (size_t r = 0; r < nRows; ++r)
+    {
+        for (size_t c = 0; c < nCols; ++c)
+        {
             sum = 0;
-            for (size_t j = 0; j < m1.C; j++) {
-                sum += m1.index[ri1 + j] * m2.index[m2.C * j + c];
+            for (size_t i = 0; i < nInner; ++i)
+            {
+                sum += m1.index[i + r * nInner] * m2T.index[i + c * nInner];
             }
-            m3.index[ri2 + c] = sum;
+            m3.index[c + r * nCols] = sum;
         }
     }
 
     // Print result
-    for (int k = 0; k < m3.R * m3.C; ++k) {
-        cout << m3.index[k] << " ";
-        if ((k + 1) % m2.C == 0) cout << endl;
-    }
+    // for (int k = 0; k < m3.R * m3.C; ++k)
+    // {
+    //     cout << m3.index[k] << " ";
+    //     if ((k + 1) % m2.C == 0)
+    //         cout << endl;
+    // }
 }
 
 int main()
@@ -155,7 +180,8 @@ int main()
     // TODO: read matrix transpose and change product
 
     // staticProduct();
-    dynamicProduct();
+
+    dynamicProduct(); // ~2.5s
 
     return 0;
 }
